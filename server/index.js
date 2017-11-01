dd58cf86071d13cdc67936d56f2ead6fd648f498
Clara Cho <clara.cho@gmail.com>
2017-11-01 16:23:02 -0700

    generate and receive live data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const db = require('../database/database');
-const sessionData = require('../data-simulation/sessionData');
 const elastic = require('../dashboard/elastic');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Tetraflix by Tetragon - User Profiles Service');
@@ -82,37 +84,11 @@ app.post('/eventsToES', (req, res) => {
   return result;
 });
 
-// TO BE MODIFIED
-// GET request to get total movie_history row count
-// generates sessions data then write into db
-// app.get('/movieHistory', (req, res) => {
-//   const sessions = sessionData.simulateData();
-//   const start = new Date();
-//   Promise.all(sessions.map((session) => {
-//     const { userId } = session;
-//     return Promise.all(session.events.map((event) => {
-//       const { id } = event.movie;
-//       const profile = JSON.stringify(event.movie.profile);
-//       const startTime = event.startTime.toLocaleString();
-//       return db.addMovieEvents({
-//         userId,
-//         id,
-//         profile,
-//         startTime,
-//       });
-//     }));
-//   }))
-//     .then(() => {
-//       const totalTime = new Date() - start;
-//       console.log(`${totalTime / 1000} seconds`);
-//       return db.countMovieHistoryRows();
-//     })
-//     .then((data) => {
-//       const { count } = data.rows[0];
-//       res.send(`Total movie events in the database: ${count}`);
-//     })
-//     .catch(e => console.error(e.stack));
-// });
+// POST request to receive live feed data
+app.post('/sessions', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
